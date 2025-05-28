d42f317f1435cd66812aaedf3337ecb59eb43cb0
EeeHello <thitiwich33@gmail.com>
2025-05-28 18:26:43 +0200

    SetUp

---
+++ Source/Lab_4/Lab_4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lab_4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lab_4, "Lab_4" );
